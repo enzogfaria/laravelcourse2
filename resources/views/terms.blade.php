ef7d5e53307ae9f36436667743ab34e7e4be7ee2
Enzo Faria <123766918+enzogfaria@users.noreply.github.com>
2024-02-11 21:56:55 -0300

    att

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.layout')
+
+@section('content')
+<h1>Terms</h1>
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam sapiente placeat nisi architecto accusantium? Quos facere nesciunt voluptates sunt. Magnam, vitae cumque! Perferendis earum sapiente porro adipisci vitae laudantium officiis.
+Doloremque provident, facilis optio eum non repudiandae, voluptate perferendis dolorum velit quas natus. Nam consequuntur, dolor, eaque quas maiores qui dicta blanditiis praesentium ad quod minus placeat perspiciatis officia pariatur.
+Vero ut, distinctio laboriosam natus impedit nesciunt alias repudiandae odit minima tempora consectetur mollitia facere nulla quam exercitationem nisi laborum voluptatem maxime assumenda, id qui autem! Dignissimos dolore similique quod.
+Reprehenderit earum quis itaque dolor excepturi? Quis blanditiis asperiores enim ducimus eaque voluptas molestiae neque exercitationem tempora officia fugit in expedita possimus nihil quo, tempore eos alias, vel necessitatibus impedit?
+Fugiat, suscipit neque fuga cupiditate expedita eos numquam sint, quis perspiciatis officiis impedit! Quia vitae iure molestiae laborum harum expedita nemo reiciendis eos repellendus, aspernatur dolor, blanditiis voluptatum modi non.</p>
+@endsection
